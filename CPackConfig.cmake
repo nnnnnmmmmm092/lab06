229dce0a7b05b92245e52063413d68f047d97b2d
Tony Stark <92053608+sk3pta@users.noreply.github.com>
2022-06-02 13:48:36 +0300

    Update CPackConfig.cmake

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -1,3 +1,4 @@
+install(TARGETS solver)
 include(InstallRequiredSystemLibraries)
 set(CPACK_PACKAGE_CONTACT donotwriteme@bmstuisbetterthanhse.com)
 set(CPACK_PACKAGE_VERSION_MAJOR ${PRINT_VERSION_MAJOR})
